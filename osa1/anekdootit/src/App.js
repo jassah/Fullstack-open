8cd6363bf003a2ff125a572603870a20ebf51e6f
Jasmin <jasmin.ahonen@gmail.com>
2022-08-01 11:32:43 +0300

    Anekdootit -tehtävä

---
+++ osa1/anekdootit/src/App.js
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [allVotes, setAll] = useState([0,0,0,0,0,0,0]) // Ei optimi tapa luoda taulukko, toimii toki pienemmmillä indeksimäärillä.
+  const copy = [...allVotes]
+
+  const vote = () => {
+    copy[selected] += 1    
+    console.log(copy)
+    setAll(copy)
+    console.log(allVotes)
+  }
+  const select = () => {
+    return (
+      setSelected(Math.floor(Math.random() * 7))
+    )
+  }
+  const Button = ({handleClick, text}) => {
+    return (
+      <button onClick={handleClick}>{text}</button>
+    ) 
+  }
+  const AnecdoteOfTheDay = () => {
+    return (
+      <>
+      <h1>Anecdote of the day</h1>
+        {anecdotes[selected]}<br />
+        has {allVotes[selected]} votes<br />
+        <Button handleClick={vote} text="VOTE"/>
+        <Button handleClick={select} text="NEXT ANECDOTE"/>
+      </>
+    )
+   }
+   const AnecdoteWithMostVotes = () => {
+    let maxValue=Math.max(...allVotes)
+    console.log(maxValue)
+    return (
+      <>
+      <h1>Anecdote with most votes</h1>
+      {anecdotes[allVotes.indexOf(maxValue)]}<br />
+      has {maxValue} votes<br />
+      </>
+    )
+   }
+  return (
+    <div>
+      <AnecdoteOfTheDay />
+      <AnecdoteWithMostVotes />
+    </div>
+  )
+}
+
+export default App
